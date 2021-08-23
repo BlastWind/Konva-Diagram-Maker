8978f64455415b2b57d9eb664bc7fa2625a85f3b
flober <andrewchen14250@gmail.com>
2021-08-23 18:02:47 -0400

    removed node_modules

--- node_modules/react-bootstrap/es/ModalTitle.js
+++
@@ -1,6 +0,0 @@
-import createWithBsPrefix from './utils/createWithBsPrefix';
-import divWithClassName from './utils/divWithClassName';
-var DivStyledAsH4 = divWithClassName('h4');
-export default createWithBsPrefix('modal-title', {
-  Component: DivStyledAsH4
-});
