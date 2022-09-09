55974fb9e5d52c6d8f53f742a938cbb9075ec058
blastwind <andrewchen14250@gmail.com>
2022-09-09 18:56:48 -0400

    Big Mistake! Commited node modules

--- node_modules/react-bootstrap/es/CarouselCaption.js
+++
@@ -1,4 +0,0 @@
-import createWithBsPrefix from './utils/createWithBsPrefix';
-export default createWithBsPrefix('carousel-caption', {
-  Component: 'div'
-});
--- node_modules/react-bootstrap/es/FigureCaption.js
+++
@@ -1,5 +0,0 @@
-import createWithBsPrefix from './utils/createWithBsPrefix';
-var FigureCaption = createWithBsPrefix('figure-caption', {
-  Component: 'figcaption'
-});
-export default FigureCaption;
